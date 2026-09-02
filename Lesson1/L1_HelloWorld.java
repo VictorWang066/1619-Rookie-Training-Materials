fe81b04b7d9648e055a1618d5ceb298a9c72d4c2
victor <victorwang777888@gmail.com>
2026-09-01 20:45:51 -0600

    filled in the answers

--- Lesson1/L1_HelloWorld.java
+++ Lesson1/L1_HelloWorld.java
@@ -1,7 +1,7 @@
 public class L1_HelloWorld {
 public static void main(String[] args) {
     // Put down your "System.out.println()" here, don't forget the ";" at the end
-
+    System.out.println("Hello World");
     
     // After you've finished, find the "Run" button on the top and select "Start Debugging"
     // See you're line on the buttom of the terminal?
--- Lesson1/L1_Variables.java
+++ Lesson1/L1_Variables.java
@@ -1,18 +1,24 @@
 public class L1_Variables {
 public static void main(String[] args) {
     // Create a string variable with an inital value of "hello" here using the keyword "String"
-
+    String my_string = "GG";
     // Create a integer variable with an initial value of 1 here using the keyword "int"
-
+    int my_integer = 1;
     // Create a double variable with an initial value of 2.0 here using the keyword "double"
-
+    double my_double = 2.0;
     // Create a boolean variable with an initial value of true here using the keyword "Boolean"
-
+    Boolean my_boolan = true;
     // Finally, create a variable with any of the above values using the keyword "var"
+    var my_variable = 33;
 
-
-    
     // Now, print out all of these variables in seperate lines using "println()"
+    System.out.println(my_string);
+    System.out.println(my_integer);
+    System.out.println(my_double);
+    System.out.println(my_boolan);
+    System.out.println(my_variable);
 
+    // After you've finished, find the "Run" button on the top and select "Start Debugging"
+    // See you're lines on the buttom of the terminal?
 }
 }
